89ae4d78eefd6193fa72b932b6cd3400ed23b99a
Yuhei Jotaki <jotaki@cloudpack.jp>
2019-04-24 08:17:58 +0900

    update: Vue本の内容に差し替え

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,143 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <product-list v-bind:products="products"></product-list>
   </div>
 </template>
 
 <script>
+import "normalize.css";
+import productList from './components/product-list.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'product-list': productList
+  },
+  data: function () {
+    return {
+      // 商品リスト
+      products: [
+        {
+          id: '01',
+          name: '紫いものビスケット',
+          price: 1580,
+          image: require("./assets/product/01.jpg"),
+          delv: 0,
+          isSale: true
+        },
+        {
+          id: '02',
+          name: '豆乳のビスケット',
+          price: 2480,
+          image: require("./assets/product/02.jpg"),
+          delv: 0,
+          isSale: false
+        },
+        {
+          id: '03',
+          name: 'かぼちゃのビスケット',
+          price: 480,
+          image: require("./assets/product/03.jpg"),
+          delv: 200,
+          isSale: false
+        },
+        {
+          id: '04',
+          name: 'カップケーキ',
+          price: 1280,
+          image: require("./assets/product/04.jpg"),
+          delv: 0,
+          isSale: true
+        },
+        {
+          id: '05',
+          name: 'トマトのカップケーキ',
+          price: 2880,
+          image: require("./assets/product/05.jpg"),
+          delv: 0,
+          isSale: false
+        },
+        {
+          id: '06',
+          name: 'プルーンのカップケーキ',
+          price: 2280,
+          image: require("./assets/product/06.jpg"),
+          delv: 0,
+          isSale: false
+        },
+        {
+          id: '07',
+          name: 'ミルクのビスケット',
+          price: 2380,
+          image: require("./assets/product/07.jpg"),
+          delv: 0,
+          isSale: false
+        },
+        {
+          id: '08',
+          name: '黒糖のビスケット',
+          price: 1680,
+          image: require("./assets/product/08.jpg"),
+          delv: 0,
+          isSale: true
+        },
+        {
+          id: '09',
+          name: 'コーンのラスク',
+          price: 1480,
+          image: require("./assets/product/09.jpg"),
+          delv: 0,
+          isSale: false
+        },
+        {
+          id: '10',
+          name: '黒糖のラスク',
+          price: 1480,
+          image: require("./assets/product/10.jpg"),
+          delv: 0,
+          isSale: false
+        },
+        {
+          id: '11',
+          name: 'みどりのラスク',
+          price: 980,
+          image: require("./assets/product/11.jpg"),
+          delv: 200,
+          isSale: true
+        },
+        {
+          id: '12',
+          name: 'きいろのラスク',
+          price: 280,
+          image: require("./assets/product/12.jpg"),
+          delv: 200,
+          isSale: false
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+html,* {
+  margin: 0;
+  padding: 0;
+}
+h1,h2,h3,h4,h5,h6 {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica Neue, Helvetica, Arial, 'Hiragino Kaku Gothic ProN', Meiryo, sans-serif;
+  -webkit-text-size-adjust: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-rendering: optimizeLegibility;
+  font-feature-settings : "palt";
+  color: #444;
+  font-size: 12px;
+  letter-spacing: .01em;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/product.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="item">
+    <ul class="icon">
+      <template v-if="product.isSale">
+        <li class="sale"><span>SALE</span></li>
+      </template>
+      <template v-if="product.delv == 0">
+        <li class="delv"><span>送料無料</span></li>
+      </template>
+      <template v-else>
+        <li class="delv"><span>送料 ¥{{product.delv | number_format}}</span></li>
+      </template>
+    </ul>
+    <figure>
+      <img v-bind:src="product.image">
+    </figure>
+    <div class="meta">
+      <h2 v-html="product.name"></h2>
+      <h3>¥{{product.price | number_format}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import './filter.js';
+
+export default {
+  name: 'product',
+  props: ['product']
+}
+</script>
+
+<style lang="scss" scoped>
+.item {
+  width: 25%;
+  padding: 50px 10px 20px;
+  border: #ddd 1px solid;
+  border-right: 0;
+  box-sizing: border-box;
+  position: relative;
+  &:last-child,
+  &:nth-child(4n) {
+    border-right: #ddd 1px solid;
+  }
+  &:nth-child(n+5) {
+    border-top: 0;
+  }
+  figure {
+    text-align: center;
+    img {
+      max-width: 120px;
+      height: auto;
+      border-radius: 4px;
+    }
+  }
+  .icon {
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    display: flex;
+    list-style: none;
+    li {
+      color: #fff;
+      font-size: 11px;
+      line-height: 1;
+      font-weight: bold;
+      border-radius: 2px;
+      margin-right: 4px;
+      &:last-child {
+        margin-right: 0;
+      }
+      span {
+        display: inline-block;
+        padding: 5px 7px;
+      }
+      &.sale {
+        background: rgb(240, 53, 53);
+      }
+      &.delv {
+        background: #999;
+      }
+    }
+  }
+  .meta {
+    margin-top: 4px;
+    text-align: center;
+    h2 {
+      color: #666;
+      font-size: 12px;
+    }
+    h3 {
+      font-size: 16px;
+    }
+  }
+}
+</style>
